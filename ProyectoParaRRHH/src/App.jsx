d2aa682043673141d3375125cb68859fc3d2377f
Nelnel03 <nelsonaraya0364@gmail.com>
2026-05-21 11:45:20 -0600

    implementacion del chat bot completamente funcional y especializado en el ambiente laboral de costa rica esperto en preguntas relacionadas a dudas sobre dicho ambiente

--- ProyectoParaRRHH/src/App.jsx
+++ ProyectoParaRRHH/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import AppRoutes from './routes/AppRoutes';
+import ChatbotWidget from './components/ChatbotWidget';
 
 function App() {
   return (
-    <AppRoutes />
+    <>
+      <AppRoutes />
+      <ChatbotWidget />
+    </>
   );
 }
 
